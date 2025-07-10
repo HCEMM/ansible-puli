5bda6ea202104a0ec24b144a6e316e73ce14396a
Joao Sequeira maildosequeira@gmail.com <maildosequeira@gmail.com>
2025-07-10 10:06:26 +0200

    Fixes in basic user creation Some fixes on user groups

--- roles/customize_puli/files/system_root/etc/profile.d/spack.sh
+++ roles/customize_puli/files/system_root/etc/profile.d/spack.sh
@@ -1,13 +1,12 @@
 # Spack environment setup for all users
 
 # Requires that the following be set on the modules.yaml file of spack (<spack_dir>/etc/spack/defaults/modules.yaml):
-#modules:
+# modules:
 #  ...
 #  default:
 #    ...
 #    enable: ["lmod"]
 
 export LMOD_QUIET=1
-source /opt/spack/share/spack/setup-env.sh
 module use /opt/spack/share/spack/lmod/linux-rocky9-x86_64/Core/
 unset LMOD_QUIET
