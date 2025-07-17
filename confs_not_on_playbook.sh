2dff3775768512e1ff9cf9e2fcf3986dcd3e279a
Joao Sequeira maildosequeira@gmail.com <maildosequeira@gmail.com>
2025-07-17 15:13:41 +0200

    Create bridge for "public" IPs of VMs Mac addresses properly formulated

--- confs_not_on_playbook.sh
+++ confs_not_on_playbook.sh
@@ -2,3 +2,30 @@
 
 setsebool -P httpd_can_network_connect 1
 
+# resize partitions
+
+# ldap connection
+dnf install -y openldap-clients
+dnf install -y sssd realmd oddjob oddjob-mkhomedir adcli samba-common-tools
+nano /etc/sssd/sssd.conf
+chmod 600 /etc/sssd/sssd.conf
+systemctl enable sssd
+systemctl start sssd
+nano /etc/nsswitch.conf         # lineinfile ->  shadow:     files sss
+authselect enable-feature with-mkhomedir
+authselect apply-changes
+nano /etc/pam.d/common-session      # lineinfile -> session required pam_mkhomedir.so skel=/etc/skel/ umask=0077
+
+
+# according to https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/9/html/configuring_authentication_and_authorization_in_rhel/configuring-sssd-to-use-ldap-and-require-tls-authentication_configuring-authentication-and-authorization-in-rhel
+dnf -y install openldap-clients sssd sssd-ldap oddjob-mkhomedir
+authselect select sssd with-mkhomedir
+cp core-dirsrv.ca.pem /etc/openldap/certs
+nano /etc/openldap/ldap.conf
+nano /etc/sssd/sssd.conf
+chmod 600 /etc/sssd/sssd.conf
+systemctl restart sssd oddjobd
+systemctl enable sssd oddjobd
+
+
+update-crypto-policies --set LEGACY
