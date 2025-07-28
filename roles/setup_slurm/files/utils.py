c5d39756ba7131fa9c4d537a082ff35e811c1f40
Joao Sequeira maildosequeira@gmail.com <maildosequeira@gmail.com>
2025-07-28 13:40:54 +0200

    Specify key file for ssh into VMs SSH configurations on local side for SSH into VMs Fixed some values in slurm.conf Fix on kickstart as well, with the new ssh configurations

--- roles/setup_slurm/files/utils.py
+++ roles/setup_slurm/files/utils.py
@@ -4,6 +4,7 @@ import os
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 def write_slurm_conf_files(excel_file=f'{script_dir}/slurm_conf.xlsx', out_dir=f'{script_dir}/../templates'):
+    print(f"Writing to {os.path.abspath(out_dir)}")
     xls = pd.ExcelFile(excel_file)
     os.makedirs(out_dir, exist_ok=True)
     for sheet in xls.sheet_names:
