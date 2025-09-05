5d77dadbc9cb96191dc62f85630919cccf70372f
Joao Sequeira maildosequeira@gmail.com <maildosequeira@gmail.com>
2025-09-05 13:33:11 +0200

    Added VM for FCBI instrument reservation Also added scripts for submitting a tumor simulation with multi-node slurm Users can now see others' jobs in Slurm

--- roles/customize_puli/files/home/hcemm/scripts/tumor_simulation/monitor.sh
+++ roles/customize_puli/files/home/hcemm/scripts/tumor_simulation/monitor.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-watch -n 2 '
+
 clear
-echo "🌡️  Tumor Simulation Dashboard - Node View"
-for i in {0..3}; do
-    echo -e "\n-----------------------------"
+while true; do
+  echo -e "\033[2J\033[H🌡️  Tumor Simulation Dashboard - Node View"
+  echo ""
+
+  for i in {0..3}; do
+    echo "-----------------------------"
     echo "Patient $i"
-    tail -n 5 patient_$i/log.txt
+    echo ""
+
+    if [[ -f patient_$i/log.txt ]]; then
+      tail -n 5 patient_$i/log.txt
+    else
+      echo "Log not found."
+    fi
+
+    echo ""
+  done
+
+  sleep 2
 done
-'
